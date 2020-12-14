0c6f4d69c2092b771de9269eaf38d3056b3a9078
Raihan <raihan@github.com>
2020-12-14 14:18:55 +0700

    coba sign up dan log in
    sign up dan log in di vue (belum memakai/mengelola token di web nya)

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -3,29 +3,78 @@
     <div class="col-md-6">
       <div class="card">
         <div class="card-header">Login</div>
-        <div class="card-body">
-          <form>
-            <div class="form-group">
-              <label for="username">Username</label>
-              <input type="email" class="form-control" placeholder="Email..">
-            </div>
-            <div class="form-group">
-              <label for="password">Password</label>
-              <input type="password" class="form-control" placeholder="Password..">
-            </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
-            <v-btn
-            color="blue darken-1"
-            cols="4"
-            text
-            >
-              <RouterLink :to="'/sign_up/'">
-                <a class="nav-link" href="#">Don't Have an Account? Sign Up Here</a>
-              </RouterLink>
-            </v-btn>
-          </form>
+        <div v-if="!submitted">
+          <div class="card-body">
+            <form>
+              <div class="form-group">
+                <label for="username">Username</label>
+                <input type="text" class="form-control" placeholder="Name.." v-model="user.username">
+              </div>
+              <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" class="form-control" placeholder="Password.." v-model="user.password">
+              </div>
+              <button @click="saveUser" type="submit" class="btn btn-primary">Submit</button>
+              <v-btn
+              color="blue darken-1"
+              cols="4"
+              text
+              >
+                <RouterLink :to="'/sign_up/'">
+                  <a class="nav-link" href="#">Don't Have an Account? Sign Up Here</a>
+                </RouterLink>
+              </v-btn>
+            </form>
+          </div>
+        </div>
+
+        <div v-else>
+          <h4>You logged in successfully!</h4>
         </div>
       </div>
     </div>
   </div>
 </template>
+
+<script>
+
+import http from "@/http";
+import "bootstrap/dist/css/bootstrap.css";
+
+export default {
+  template: '#sign-in',
+  name: "sign-in",
+  data() {
+    return {
+      user: {
+        username: "",
+        password: "",
+      },
+      submitted: false,
+    };
+  },
+  methods: {
+    saveUser() {
+      var data = {
+        username: this.user.username,
+        password: this.user.password,
+      };
+
+      http.post('http://localhost:8000/api/users/signin', data)
+        .then(response => {
+          console.log(response.data);
+          //console.log(data.is_admin)
+          this.submitted = true;
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    },
+    
+    // newUser() {
+    //   this.submitted = false;
+    //   this.user = {};
+    // }
+  }
+};
+</script>
--- frontend/src/components/SignUp.vue
+++ frontend/src/components/SignUp.vue
@@ -5,24 +5,77 @@
         <div class="card-header">
           Sign Up
         </div>
-        <div class="card-body">
-          <form>
-            <div class="form-group">
-              <label for="username">Username</label>
-              <input type="text" class="form-control" placeholder="Name..">
-            </div>
-            <div class="form-group">
-              <label for="email">Email address</label>
-              <input type="email" class="form-control" placeholder="Email..">
-            </div>
-            <div class="form-group">
-              <label for="password">Password</label>
-              <input type="password" class="form-control" placeholder="Password..">
-            </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
-          </form>
+        <div v-if="!submitted">
+          <div class="card-body">
+            <form>
+              <div class="form-group">
+                <label for="username">Username</label>
+                <input type="text" class="form-control" placeholder="Name.." v-model="user.username">
+              </div>
+              <div class="form-group">
+                <label for="email">Email address</label>
+                <input type="email" class="form-control" placeholder="Email.." v-model="user.email">
+              </div>
+              <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" class="form-control" placeholder="Password.." v-model="user.password">
+              </div>
+              <button @click="saveUser" type="submit" class="btn btn-primary">Submit</button>
+            </form>
+          </div>
+        </div>
+
+        <div v-else>
+          <h4>You submitted successfully!</h4>
         </div>
       </div>
     </div>
   </div>
 </template>
+
+<script>
+
+import http from "@/http";
+import "bootstrap/dist/css/bootstrap.css";
+
+export default {
+  template: '#sign-up',
+  name: "sign-up",
+  data() {
+    return {
+      user: {
+        username: "",
+        email: "",
+        password: "",
+        is_admin: false
+      },
+      submitted: false,
+    };
+  },
+  methods: {
+    saveUser() {
+      var data = {
+        username: this.user.username,
+        email: this.user.email,
+        password: this.user.password,
+        is_admin: this.user.is_admin
+      };
+
+      http.post('http://localhost:8000/api/users/signup', data)
+        .then(response => {
+          console.log(response.data);
+          console.log(data.is_admin)
+          this.submitted = true;
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    },
+    
+    newUser() {
+      this.submitted = false;
+      this.user = {};
+    }
+  }
+};
+</script>
