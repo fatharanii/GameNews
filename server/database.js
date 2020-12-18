9da8134cbf8d88613b062c13167eeaedcd6de170
Fadhil2912 <muhammad.fadhil.tif418@polban.ac.id>
2020-12-18 15:37:44 +0700

    Update database.js

--- server/database.js
+++ server/database.js
@@ -1,9 +1,12 @@
 const {Client} = require ('pg')
 //const port = 8000
-const connection = "postgressql://postgres:admin123@localhost:5432/GameNews"
+const connection = "postgres://lcczsojytqgsru:6008c425f1b5441c1e67a72fe0990b8be7a455fd708879e5e3b33becc2326368@ec2-54-205-26-79.compute-1.amazonaws.com:5432/d5cu7fq0a05tcd"
 
 const client = new Client({
-    connectionString: connection
+    connectionString: connection,
+        ssl:{
+	        rejectUnauthorized: false
+        }
     })
 
 client.connect()
