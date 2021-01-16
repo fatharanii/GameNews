ec27854707e467acce09714c8e61796cf1a70c59
HanifahSho <hanifah.sholihat.tif418@polban.ac.id>
2021-01-16 14:06:22 -0800

    Benerin Performance "Aktifkan Kompresi Teks"

--- server/index.js
+++ server/index.js
@@ -23,7 +23,8 @@ app.use(cors())
 app.use(function(req, res, next) {
     res.header(
       "Access-Control-Allow-Headers",
-      "x-access-token, Origin, Content-Type, Accept"
+      "x-access-token, Origin, Content-Type, Accept",
+      "Accept-Encoding: * "
     );
     next();
   });
