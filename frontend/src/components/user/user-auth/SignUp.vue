8057333c601a960dfe919c0dcb240b238799f39e
Raihan <raihan@github.com>
2021-01-18 17:46:19 +0700

    Nambahin confirm password di sign up

--- frontend/src/components/user/user-auth/SignUp.vue
+++ frontend/src/components/user/user-auth/SignUp.vue
@@ -9,15 +9,31 @@
             <form>
               <div class="form-group">
                 <label for="username">Username</label>
-                <input type="text" class="form-control" placeholder="Name.." v-model="user.username">
+                <validation-provider rules="required" v-slot="{ errors }">
+                  <input type="text" class="form-control" placeholder="Name.." v-model="user.username">
+                  <span style="color:red">{{ errors[0] }}</span>
+                </validation-provider>
               </div>
               <div class="form-group">
                 <label for="email">Email address</label>
-                <input type="email" class="form-control" placeholder="Email.." v-model="user.email">
+                <validation-provider rules="required" v-slot="{ errors }">
+                  <input type="email" class="form-control" placeholder="Email.." v-model="user.email">
+                  <span style="color:red">{{ errors[0] }}</span>
+                </validation-provider>
               </div>
               <div class="form-group">
                 <label for="password">Password</label>
-                <input type="password" class="form-control" placeholder="Password.." v-model="user.password">
+                <validation-provider rules="required" v-slot="{ errors }">
+                  <input type="password" class="form-control" placeholder="Password.." v-model="user.password">
+                  <span style="color:red">{{ errors[0] }}</span>
+                </validation-provider>
+              </div>
+              <div class="form-group">
+                <label for="password">Masukkan Ulang Password</label>
+                <validation-provider rules="required" v-slot="{ errors }">
+                  <input type="password" class="form-control" placeholder="Password.." v-model="reenteredPassword">
+                  <span style="color:red">{{ errors[0] }}</span>
+                </validation-provider>
               </div>
               <RouterLink :to="'/sign_up/'">
                   <a href="#"><button @click="saveUser" type="submit" class="btn btn-primary">Submit</button></a>
@@ -43,6 +59,26 @@
               class="text-center text-subtitle-2">
               You have registered successfully!
             </v-alert>
+
+          <v-alert 
+              dense
+              outlined
+              type="error"
+              :icon="false"
+              v-if="differentPassword"
+              class="text-center text-subtitle-2">
+              Masukkan password yang sama dengan password yang anda masukkan sebelumnya
+            </v-alert>
+
+            <v-alert 
+              dense
+              outlined
+              type="error"
+              :icon="false"
+              v-if="dataIsNotCompleted"
+              class="text-center text-subtitle-2">
+              Lengkapi data terlebih dahulu
+            </v-alert>
       </div>
     </div>
   </div>
@@ -53,7 +89,16 @@
 // import http from "@/http";
 import "bootstrap/dist/css/bootstrap.css";
 import UserDataService from "../../../services/UserDataService";
+import { ValidationProvider, extend } from 'vee-validate';
+import { required } from 'vee-validate/dist/rules';
+extend('required', {
+  ...required,
+  message: 'This field is required'
+});
 export default {
+  components: {
+    ValidationProvider
+  },
   template: '#sign-up',
   name: "sign-up",
   data() {
@@ -67,9 +112,17 @@ export default {
       submitted: false,
       isError: false,
       errorMessage: "",
+      reenteredPassword: "",
+      differentPassword: false,
+      dataIsNotCompleted: false
     };
   },
   methods: {
+    clearErrorMessage() {
+      this.isError = false;
+      this.differentPassword = false;
+      this.dataIsNotCompleted = false;
+    },
     saveUser() {
       var data = {
         username: this.user.username,
@@ -77,28 +130,49 @@ export default {
         password: this.user.password,
         is_admin: this.user.is_admin
       };
+      var passwordVerification = this.reenteredPassword;
 
-      UserDataService.signUp(data)
-        .then(response => {
-          if(response.data.message != "registered sucessfully") {
-            this.submitted = false;
-            this.isError = true;
-            this.errorMessage = response.data.message;
-            console.log(response.data.message);
-            console.log(response.data);
-            console.log(data.is_admin)
-          }
-          else {
-            console.log(response.data);
-            console.log(data.is_admin)
-            this.submitted = true;
-            this.isError = false;
-            this.$router.push({path: '/login/'});
-          }
-        })
-        .catch(e => {
-          console.log(e);
-        });
+      if(this.user.username=="" || this.user.email=="" || this.user.password==""
+       || this.reenteredPassword=="") {
+         this.clearErrorMessage();
+         this.dataIsNotCompleted = true;
+      }
+      else {
+        this.dataIsNotCompleted = false;
+        if(this.user.password != passwordVerification) {
+          this.clearErrorMessage();
+          this.differentPassword = true;
+        }
+        else {
+          this.differentPassword = false;
+          UserDataService.signUp(data)
+            .then(response => {
+              if(response.data.message != "registered sucessfully") {
+                this.submitted = false;
+                this.isError = true;
+                this.errorMessage = response.data.message;
+                console.log(response.data.message);
+                console.log(response.data);
+                console.log(data.is_admin)
+              }
+              else {
+                console.log(response.data);
+                console.log(data.is_admin)
+                this.submitted = true;
+                this.isError = false;
+                this.$router.push({path: '/login/'});
+              }
+            })
+            .catch(e => {
+              console.log(e);
+            });
+        }
+      }
+
+      
+      
+
+      
     },
     
     // newUser() {
